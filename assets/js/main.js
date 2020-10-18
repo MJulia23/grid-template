068875b25456c1fd3527f7697807dacc867e5f3a
Julia Makarenko <crazy.romanuk@gmail.com>
2020-10-18 22:09:09 +0300

    btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+$(document).ready(function() {
+    $('.header-navbar__btn').on('click', function() {
+        $(this).toggleClass('header-navbar__btn--active');
+        $('.header-navbar__list').stop(true, true).fadeToggle(500);
+    });
 });
+
+let $window = $(window);
+
+$window.resize(function() {
+    if ($window.width() > 768) {
+        $('.header-navbar__list').removeAttr('style')
+        $('.header-navbar__btn').removeClass('header-navbar__btn--active');
+    }
+})
